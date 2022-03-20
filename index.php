3e336108f0bcc8ca0cbc10babb2c1e0be38c4543
Goswami Jaydeep <46455030+IronSpydi@users.noreply.github.com>
2022-03-20 08:39:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$to_email = "gajjjar22@gmail.com";
+$to_email = "recipient@gmail.com";
 $subject = "test mail";
 $body = "hellow";
-$headers = "From: gajjjar22@gmail.com";
+$headers = "From: your-gmail@gmail.com";
 
 if (mail($to_email, $subject, $body, $headers)) {
     echo "Email successfully sent to $to_email...";
